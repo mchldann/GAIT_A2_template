7d08e8bff22592cd2478ef710034c24a0889388a
Benjamin Frye <ben.j.frye@gmail.com>
2020-09-22 12:10:45 +1000

    Modify sheep mechanics.

--- Assets/Scripts/Sheep.cs
+++ Assets/Scripts/Sheep.cs
@@ -54,7 +54,7 @@ namespace Completed
 		}
 		
 		
-		//MoveSheep is called by the GameManger each turn to tell each Sheep to try to move towards the player.
+		//MoveSheep is called by the GameManger each turn to tell each Sheep to try to away from the player.
 		public void MoveSheep ()
 		{
 			//Declare variables for X and Y axis move directions, these range from -1 to 1.
@@ -62,16 +62,13 @@ namespace Completed
 			int xDir = 0;
 			int yDir = 0;
 			
-			//If the difference in positions is approximately zero (Epsilon) do the following:
-			if(Mathf.Abs (target.position.x - transform.position.x) < float.Epsilon)
-				
-				//If the y coordinate of the target's (player) position is greater than the y coordinate of this sheep's position set y direction 1 (to move up). If not, set it to -1 (to move down).
-				yDir = target.position.y > transform.position.y ? 1 : -1;
-			
-			//If the difference in positions is not approximately zero (Epsilon) do the following:
+			float xDiff = target.position.x - transform.position.x;
+			float yDiff = target.position.y - transform.position.y;
+
+			if (Mathf.Abs(xDiff) < Mathf.Abs(yDiff))
+				xDir = xDiff > 0.0f ? -1 : 1;
 			else
-				//Check if target x position is greater than sheep's x position, if so set x direction to 1 (move right), if not set to -1 (move left).
-				xDir = target.position.x > transform.position.x ? 1 : -1;
+				yDir = yDiff > 0.0f ? -1 : 1;
 			
 			//Call the AttemptMove function and pass in the generic parameter Player, because Sheep is moving and expecting to potentially encounter a Player
 			AttemptMove <Player> (xDir, yDir);
